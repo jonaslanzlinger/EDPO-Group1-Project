45e4cb883050da0b7782bb6bf1116cc06bafadb5
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-03-23 20:46:38 +0100

    delivery service

--- delivery/src/main/java/ch/unisg/delivery/services/DeliveryProcessingService.java
+++ delivery/src/main/java/ch/unisg/delivery/services/DeliveryProcessingService.java
@@ -1,4 +1,4 @@
-package ch.unisg.grabber.worker;
+package ch.unisg.delivery.services;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
@@ -6,32 +6,38 @@ import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
-
 @Service
-public class GrabberProcessingService {
+public class DeliveryProcessingService {
 
     private final ZeebeClient zeebeClient;
 
     @Autowired
-    public GrabberProcessingService(ZeebeClient zeebeClient) {
+    public DeliveryProcessingService(ZeebeClient zeebeClient) {
         this.zeebeClient = zeebeClient;
     }
 
-    @ZeebeWorker(type = "grabGoods", name = "grabGoodsProcessor")
-    public void grabGoods(final ActivatedJob job) {
+    @ZeebeWorker(type = "deliverGoods", name = "deliverGoodsProcessor")
+    public void deliverGoods(final ActivatedJob job) {
         String orderDetails = job.getVariablesAsMap().get("orderDetails").toString();
+
+        System.out.println("Processing order: " + orderDetails);
 
         String variables = String.format("{\"orderDetails\": \"%s\"}", orderDetails);
 
-        if (Math.random() < 0.5){
+        if (orderDetails.contains("blue")) {
+            System.out.println("New Throw Error Command: DeliveryStationError for process instance " + job.getProcessInstanceKey() + " with key " + job.getKey() + " orderDetails: " + orderDetails);
+            zeebeClient.newThrowErrorCommand(job.getKey())
+                    .errorCode("DeliveryStationError")
+                    .errorMessage(String.format("Orders with %s goods fail in delivery station", orderDetails))
+                    .send()
+                    .join(); // Synchronous throwing, remove join() for asynchronous
+        } else {
             System.out.println("New Complete Command: process instance " + job.getProcessInstanceKey() + " with key " + job.getKey() + " orderDetails: " + orderDetails);
             zeebeClient.newCompleteCommand(job.getKey())
                     .variables(variables)
                     .send()
                     .join(); // Synchronous completion, remove join() for asynchronous
-        } else {
-            System.out.println("Wait for timeout: process instance " + job.getProcessInstanceKey() + " with key " + job.getKey() + " orderDetails: " + orderDetails);
         }
     }
 }
+
