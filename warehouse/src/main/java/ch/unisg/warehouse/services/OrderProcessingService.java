3a80024b47c8514592d23ccee019c359345be799
KaiTries <kai.schultz@bluewin.ch>
2024-03-23 12:22:04 +0100

    renamed

--- warehouse/src/main/java/ch/unisg/warehouse/services/OrderProcessingService.java
+++ warehouse/src/main/java/ch/unisg/warehouse/services/OrderProcessingService.java
@@ -1,11 +1,20 @@
 package ch.unisg.warehouse.services;
 
+import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderProcessingService {
+
+    private final ZeebeClient zeebeClient;
+
+    @Autowired
+    public OrderProcessingService(ZeebeClient zeebeClient) {
+        this.zeebeClient = zeebeClient;
+    }
 
     @ZeebeWorker(type = "checkGoods", name = "checkGoodsProcessor")
     public void processOrder(final ActivatedJob job) {
@@ -16,5 +25,13 @@ public class OrderProcessingService {
         System.out.println("Processing order: " + orderDetails);
 
         // Optionally update process variables or complete the job with new variables
+        String resultVariable = "{\"result\": \"Success\"}";
+
+        // Complete the job
+        zeebeClient.newCompleteCommand(job.getKey())
+                .variables(resultVariable)
+                .send()
+                .join(); // Synchronous completion, remove join() for asynchronous
     }
 }
+
