893406184272747a5b23623abf049d503fd27ed0
KaiTries <kai.schultz@bluewin.ch>
2024-03-31 16:53:08 +0200

    added orderId,orderColor and deliveryMethod to entire process as env variables

--- order/src/main/java/ch/unisg/order/rest/ShopRestController.java
+++ order/src/main/java/ch/unisg/order/rest/ShopRestController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.Map;
 import java.util.Random;
 
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
