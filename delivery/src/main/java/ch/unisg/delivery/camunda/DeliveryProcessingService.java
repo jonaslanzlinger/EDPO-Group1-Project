ed09454b528bf3086d86d17c7b0bbd8142ebdae4
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-04-01 18:19:35 +0200

    implemented hystrix circuit breaker in DeliveryProcessingService

--- delivery/src/main/java/ch/unisg/delivery/camunda/DeliveryProcessingService.java
+++ delivery/src/main/java/ch/unisg/delivery/camunda/DeliveryProcessingService.java
@@ -3,12 +3,22 @@ package ch.unisg.delivery.camunda;
 import ch.unisg.delivery.domain.Order;
 import ch.unisg.delivery.domain.OrderRegistry;
 import ch.unisg.delivery.utils.WorkflowLogger;
+import com.google.api.Http;
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.sql.SQLOutput;
 import java.util.Map;
 
 /**
@@ -50,15 +60,51 @@ public class DeliveryProcessingService {
         WorkflowLogger.info(log, "registerOrder","Complete order: " + job.getProcessInstanceKey() + " - " + orderColor);
 
         camundaMessageSenderService.sendCompleteCommand(job.getKey(), job.getVariables());
+    }
 
-//        if (orderColor.contains("blue")) {
-//            WorkflowLogger.info(log, "registerOrder","Failed order: " + job.getProcessInstanceKey() + " - " + orderColor);
-//            camundaMessageSenderService.throwErrorCommand("DeliveryStationError",
-//                    String.format("Orders with %s goods fail in delivery station", orderColor), job.getKey());
-//        } else {
-//            WorkflowLogger.info(log, "registerOrder","Complete order: " + job.getProcessInstanceKey() + " - " + orderColor);
-//            camundaMessageSenderService.sendCompleteCommand(job.getKey(), variables);
-//        }
+    @ZeebeWorker(type = "retrieveColor", name = "retrieveColorProcessor")
+    public void retrieveColor(final ActivatedJob job) {
+
+        // TODO: Leave this, maybe we need this later
+//        Map<String, Object> orderVariables = (Map<String, Object>) job.getVariablesAsMap().get("order");
+//
+//        String orderId = (String) orderVariables.get("orderId");
+//        String orderColor = (String) orderVariables.get("orderColor");
+//        String deliveryMethod = (String) orderVariables.get("deliveryMethod");
+
+        WorkflowLogger.info(log, "retrieveColor","Retrieving color at light sensor...");
+
+        // Wrap calling the color REST endpoint in the factory within a Hystrix Circuit breaker
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://factorysimulator:8085/vgr/read_color"))
+                .GET()
+                .build();
+
+        HystrixCommand.Setter config = HystrixCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("retrieveColor"))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        .withExecutionTimeoutInMilliseconds(2_000));
+
+        HttpResponse response = new HystrixCommand<HttpResponse>(config) {
+
+            @Override
+            protected HttpResponse<String> run() throws Exception {
+                HttpClient client = HttpClient.newHttpClient();
+
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+                WorkflowLogger.info(log, "retrieveColor","Color retrieved: " + response.body());
+
+                camundaMessageSenderService.sendCompleteCommand(job.getKey(), job.getVariables());
+                return null;
+            }
+
+            @Override
+            protected HttpResponse<String> getFallback() {
+                WorkflowLogger.info(log, "retrieveColor","Circuit breaker - Fallback occurred.");
+
+                camundaMessageSenderService.throwErrorCommand("FactoryTimeoutError", "Error retrieving color at light sensor.", job.getKey());
+                return null;
+            }
+        }.execute();
     }
 }
-
