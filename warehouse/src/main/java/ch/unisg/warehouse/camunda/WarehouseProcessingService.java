4494dc8132e7b2851c425191750bc1b101d564cb
KaiTries <kai.schultz@bluewin.ch>
2024-04-03 17:11:41 +0200

    improved some methods

--- warehouse/src/main/java/ch/unisg/warehouse/camunda/WarehouseProcessingService.java
+++ warehouse/src/main/java/ch/unisg/warehouse/camunda/WarehouseProcessingService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+import static ch.unisg.warehouse.utils.Utility.getFromMap;
+
 /**
  * This is a service class that processes warehouse tasks.
  * It uses the CamundaService to interact with the Camunda engine.
@@ -23,10 +25,6 @@ public class WarehouseProcessingService {
 
     private final WarehouseService warehouseService;
 
-    /**
-     * Constructor for the WarehouseProcessingService class.
-     * @param camundaMessageSenderService The CamundaService instance to be used for interactions with the Camunda engine.
-     */
     @Autowired
     public WarehouseProcessingService(CamundaService camundaMessageSenderService, WarehouseService warehouseService) {
         this.camundaMessageSenderService = camundaMessageSenderService;
@@ -37,102 +35,113 @@ public class WarehouseProcessingService {
      * This method checks the goods in the warehouse.
      * If the goods are not available, it throws an error command to the Camunda engine.
      * If the goods are available, it sends a complete command to the Camunda engine.
+     * It also sends the product slot to the Camunda engine.
+     *
      * @param job The job that contains the details of the order.
      */
     @ZeebeWorker(type = "checkGoods", name = "checkGoodsProcessor")
     public void checkGoods(final ActivatedJob job) {
-        Map<String, Object> orderVariables = (Map<String, Object>) job.getVariablesAsMap().get("order");
-        // Now you can access individual properties within the 'order' object
-        String orderColor = (String) orderVariables.get("orderColor");
-
-        WorkflowLogger.info(log, "checkGoods","Processing order: " + job.getProcessInstanceKey() + " - " + orderColor);
-
+        Map<String, Object> order = getFromMap(job.getVariablesAsMap(), "order", Map.class);
+        String orderColor = getFromMap(order, "orderColor", String.class);
         String productId = warehouseService.getProductSlot(orderColor);
 
-        String productSlot = String.format("{\"productSlot\":\"%s\"}", productId);
+        WorkflowLogger.info(log, "checkGoods","Processing order: "
+                + job.getProcessInstanceKey() + " - " + orderColor);
+
 
         if (productId == null) {
-            WorkflowLogger.info(log, "checkGoods", "Failed Order: " + job.getProcessInstanceKey()+ " - " + orderColor);
+            WorkflowLogger.info(log, "checkGoods", "Failed Order: "
+                    + job.getProcessInstanceKey()+ " - " + orderColor);
             camundaMessageSenderService.throwErrorCommand("GoodsNotAvailable",
                     String.format("No %s goods available", orderColor), job.getKey());
         } else {
-            WorkflowLogger.info(log, "checkGoods", "Complete order: " + job.getProcessInstanceKey()+ " - " + orderColor);
-            camundaMessageSenderService.sendCompleteCommand(job.getKey(), productSlot);
+            WorkflowLogger.info(log, "checkGoods", "Complete order: "
+                    + job.getProcessInstanceKey()+ " - " + orderColor);
+            camundaMessageSenderService.sendCompleteCommand(job.getKey(),
+                    String.format("{\"productSlot\":\"%s\"}", productId));
         }
     }
 
+
+    /**
+     * This method checks if the goods are available in the warehouse.
+     * If the goods are not available, it throws an error command to the Camunda engine.
+     * If the goods are available, it sends a complete command to the Camunda engine.
+     *
+     * @param job The job that contains the details of the order.
+     */
     @ZeebeWorker(type = "checkGoodsAvailable", name = "checkGoodsAvailableProcessor")
     public void checkGoodsAvailable(final ActivatedJob job) {
-        Map<String, Object> orderVariables = (Map<String, Object>) job.getVariablesAsMap().get("order");
-        // Now you can access individual properties within the 'order' object
-        String orderColor = (String) orderVariables.get("orderColor");
-
-        WorkflowLogger.info(log, "checkGoodsAvailable","Processing order: " + job.getProcessInstanceKey() + " - " + orderColor);
-
-
+        Map<String, Object> order = getFromMap(job.getVariablesAsMap(), "order", Map.class);
+        String orderColor = getFromMap(order, "orderColor", String.class);
         boolean isAvailable = warehouseService.checkProduct(orderColor);
 
+        WorkflowLogger.info(log, "checkGoodsAvailable","Processing order: "
+                + job.getProcessInstanceKey() + " - " + orderColor);
+
         if (!isAvailable) {
-            WorkflowLogger.info(log, "checkGoodsAvailable", "Failed Order: " + job.getProcessInstanceKey()+ " - " + orderColor);
+            WorkflowLogger.info(log, "checkGoodsAvailable", "Failed Order: "
+                    + job.getProcessInstanceKey()+ " - " + orderColor);
             camundaMessageSenderService.throwErrorCommand("GoodsNotAvailable",
                     String.format("No %s goods available", orderColor), job.getKey());
         } else {
-            WorkflowLogger.info(log, "checkGoodsAvailable", "Complete order: " + job.getProcessInstanceKey()+ " - " + orderColor);
+            WorkflowLogger.info(log, "checkGoodsAvailable", "Complete order: "
+                    + job.getProcessInstanceKey()+ " - " + orderColor);
             camundaMessageSenderService.sendCompleteCommand(job.getKey(), job.getVariables());
         }
     }
 
     @ZeebeWorker(type = "checkHBW", name = "checkHBWProcessor")
     public void checkHBWStatus(final ActivatedJob job) {
-        WorkflowLogger.info(log, "checkHBWStatus", "Checking HBW status");
-        Map<String, Object> orderVariables = (Map<String, Object>) job.getVariablesAsMap().get("order");
-        // Now you can access individual properties within the 'order' object
-        String orderId = (String) orderVariables.get("orderId");
+        Map<String, Object> order = getFromMap(job.getVariablesAsMap(), "order", Map.class);
+        String orderId = getFromMap(order, "orderId", String.class);
         boolean inUse = warehouseService.isInUse();
-        String inUseJson = String.format("{\"available\":\"%s\"}", !inUse);
+
+        WorkflowLogger.info(log, "checkHBWStatus", "Checking HBW status");
 
         if (inUse) {
             WorkflowLogger.info(log, "checkHBWStatus", "HBW is in use");
-            camundaMessageSenderService.sendCompleteCommand(job.getKey(),inUseJson);
+            camundaMessageSenderService.sendCompleteCommand(job.getKey(),"{\"available\":\"false\"}");
             warehouseService.addToQueue(orderId);
         } else {
             WorkflowLogger.info(log, "checkHBWStatus", "HBW is not in use");
-            camundaMessageSenderService.sendCompleteCommand(job.getKey(), inUseJson);
+            camundaMessageSenderService.sendCompleteCommand(job.getKey(), "{\"available\":\"true\"}");
         }
     }
 
     @ZeebeWorker(type = "lockHBW", name = "lockHBWProcessor")
     public void lockHBW(final ActivatedJob job) {
-        WorkflowLogger.info(log, "lockHBW", "Locking HBW");
-        Map<String, Object> orderVariables = (Map<String, Object>) job.getVariablesAsMap().get("order");
-        // Now you can access individual properties within the 'order' object
-        String orderId = (String) orderVariables.get("orderId");
-
+        Map<String, Object> order = getFromMap(job.getVariablesAsMap(), "order", Map.class);
+        String orderId = getFromMap(order, "orderId", String.class);
         boolean success = warehouseService.setInUse();
-        String lockedJson = String.format("{\"locked\":\"%s\"}", success);
+
+        WorkflowLogger.info(log, "lockHBW", "Locking HBW");
 
         if (success) {
             WorkflowLogger.info(log, "lockHBW", "HBW locked");
-            camundaMessageSenderService.sendCompleteCommand(job.getKey(), lockedJson);
+            camundaMessageSenderService.sendCompleteCommand(job.getKey(), "{\"locked\":\"true\"}");
         } else {
             WorkflowLogger.info(log, "lockHBW", "HBW already in use");
-            camundaMessageSenderService.sendCompleteCommand(job.getKey(), lockedJson);
+            camundaMessageSenderService.sendCompleteCommand(job.getKey(), "{\"locked\":\"false\"}");
             warehouseService.addToQueue(orderId);
         }
     }
 
     @ZeebeWorker(type = "freeHBW", name = "freeHBWProcessor")
     public void freeHBW(final ActivatedJob job) {
+
         WorkflowLogger.info(log, "freeHBW", "Freeing HBW");
-
         String nextProcess = warehouseService.releaseHBW();
-        String freedJson = String.format("{\"available\":\"%s\"}", true);
         WorkflowLogger.info(log, "freeHBW", "HBW freed");
-        camundaMessageSenderService.sendCompleteCommand(job.getKey(), freedJson);
 
+        camundaMessageSenderService.sendCompleteCommand(job.getKey(), "{\"available\":\"true\"}");
         if (nextProcess != null) {
             WorkflowLogger.info(log, "freeHBW", "Next process: " + nextProcess);
-            camundaMessageSenderService.sendMessageCommand("HBWAvailable",nextProcess,freedJson);
+            camundaMessageSenderService.sendMessageCommand(
+                    "HBWAvailable",
+                    nextProcess,
+                    "{\"available\":\"true\"}"
+            );
         }
     }
 
@@ -142,5 +151,6 @@ public class WarehouseProcessingService {
         String productSlot = job.getVariablesAsMap().get("productSlot").toString();
         warehouseService.getProduct(productSlot);
         camundaMessageSenderService.sendCompleteCommand(job.getKey(), job.getVariables());
+        WorkflowLogger.info(log, "unloadProduct", "unloaded product");
     }
 }
