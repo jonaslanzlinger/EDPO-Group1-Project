71fa467dcad0e642bc507c063f97e3049166627b
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-05-19 12:53:10 +0200

    improved deserialization

--- monitoring/src/main/java/ch/unisg/monitoring/kafka/topology/ProcessingTopology.java
+++ monitoring/src/main/java/ch/unisg/monitoring/kafka/topology/ProcessingTopology.java
@@ -1,24 +1,15 @@
 package ch.unisg.monitoring.kafka.topology;
 
-import ch.unisg.monitoring.domain.stations.HBW_1;
-import ch.unisg.monitoring.domain.stations.VGR_1;
-
-import ch.unisg.monitoring.serialization.FactoryEvent;
 import ch.unisg.monitoring.serialization.HbwEvent;
 import ch.unisg.monitoring.serialization.VgrEvent;
-import ch.unisg.monitoring.serialization.json.FactoryEventSerdes;
-import ch.unisg.monitoring.serialization.json.hbw.HbwDeserializer;
-import ch.unisg.monitoring.serialization.json.vgr.VgrDeserializer;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
+import ch.unisg.monitoring.serialization.json.hbw.HbwEventSerdes;
+import ch.unisg.monitoring.serialization.json.vgr.VgrEventSerdes;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
+
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.state.KeyValueStore;
-import org.apache.kafka.streams.state.StoreBuilder;
-import org.apache.kafka.streams.state.Stores;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.nio.charset.StandardCharsets;
@@ -27,41 +18,19 @@ import java.util.Arrays;
 
 
 public class ProcessingTopology {
-    private static final Gson gsonHBW = new GsonBuilder()
-            .registerTypeAdapter(HBW_1 .class, new HbwDeserializer())
-            .create();
-    private static final Gson gsonVGR = new GsonBuilder()
-            .registerTypeAdapter(VGR_1.class, new VgrDeserializer())
-            .create();
     public static Topology build() {
 
         StreamsBuilder builder = new StreamsBuilder();
 
-        StoreBuilder<KeyValueStore<byte[], VgrEvent>> storeBuilderVGR =
-                Stores.keyValueStoreBuilder(
-                        Stores.persistentKeyValueStore("previous-event-store-vgr"),
-                        Serdes.ByteArray(),
-                        new JsonSerde<>(VgrEvent.class)
-                );
-        StoreBuilder<KeyValueStore<byte[], HbwEvent>> storeBuilderHBW =
-                Stores.keyValueStoreBuilder(
-                        Stores.persistentKeyValueStore("previous-event-store-hbw"),
-                        Serdes.ByteArray(),
-                        new JsonSerde<>(HbwEvent.class)
-                );
 
-
-        builder.addStateStore(storeBuilderVGR);
-        builder.addStateStore(storeBuilderHBW);
-
-        KStream<byte[], FactoryEvent> stream = builder.stream("monitoring-all", Consumed.with(Serdes.ByteArray(), new FactoryEventSerdes()));
+        KStream<byte[], byte[]> stream = builder.stream("monitoring-all", Consumed.with(Serdes.ByteArray(), Serdes.ByteArray()));
 
         // DEBUG
-        // stream.print(Printed.<byte[], FactoryEvent>toSysOut().withLabel("monitoring-all"));
-        // KStream<byte[], FactoryEvent> peekedStream = stream.peek((key, value) -> System.out.println("Key:
+        // stream.print(Printed.<byte[], byte[]>toSysOut().withLabel("monitoring-all"));
+        // KStream<byte[], byte[]> peekedStream = stream.peek((key, value) -> System.out.println("Key:
         // " + Arrays.toString(key)));
 
-        KStream<byte[], FactoryEvent>[] branches = stream.branch(
+        KStream<byte[], byte[]>[] branches = stream.branch(
                 (key, value) -> Arrays.equals(key, "VGR_1".getBytes(StandardCharsets.UTF_8)),
                 (key, value) -> Arrays.equals(key, "HBW_1".getBytes(StandardCharsets.UTF_8))
         );
@@ -69,34 +38,17 @@ public class ProcessingTopology {
         // DEBUG: check if the branch is working
         // branches[0].print(Printed.<byte[], FactoryEvent>toSysOut().withLabel("VGR_1"));
         // branches[1].print(Printed.<byte[], FactoryEvent>toSysOut().withLabel("HBW_1"));
+        HbwEventSerdes hbwEventSerdes = new HbwEventSerdes();
+        VgrEventSerdes vgrEventSerdes = new VgrEventSerdes();
 
-        KStream<byte[], VgrEvent> vgrTypedStream =  branches[0].mapValues(v -> {
-            VgrEvent vgrEvent = new VgrEvent();
-            vgrEvent.setId(v.getId());
-            vgrEvent.setSource(v.getSource());
-            vgrEvent.setTime(v.getTime());
-            vgrEvent.setDatacontenttype(v.getDatacontenttype());
-            vgrEvent.setSpecversion(v.getSpecversion());
-            // Deserialize the data field to VGR_1
-            String jsonData = gsonVGR.toJson(v.getData());
-            VGR_1 vgrData = gsonVGR.fromJson(jsonData, VGR_1.class);
-            vgrEvent.setData(vgrData);
-            return vgrEvent;
-        });
 
-        KStream<byte[], HbwEvent> hbwTypedStream =  branches[1].mapValues(v -> {
-            HbwEvent hbwEvent = new HbwEvent();
-            hbwEvent.setId(v.getId());
-            hbwEvent.setTime(v.getTime());
-            hbwEvent.setSpecversion(v.getSpecversion());
-            hbwEvent.setSource(v.getSource());
-            hbwEvent.setDatacontenttype(v.getDatacontenttype());
-            // Deserialize the data field to HBW_1
-            String jsonData = gsonHBW.toJson(v.getData());
-            HBW_1 hbwData = gsonHBW.fromJson(jsonData, HBW_1.class);
-            hbwEvent.setData(hbwData);
-            return hbwEvent;
-        });
+        KStream<byte[], VgrEvent> vgrTypedStream =  branches[0].mapValues(v ->
+                vgrEventSerdes.deserializer().deserialize("VGR_1", v)
+        );
+
+        KStream<byte[], HbwEvent> hbwTypedStream =  branches[1].mapValues(v ->
+                hbwEventSerdes.deserializer().deserialize("HBW_1", v)
+        );
 
         // DEBUG: print vgrTypedStream to console
         vgrTypedStream.print(Printed.<byte[], VgrEvent>toSysOut().withLabel("vgrTypedStream"));
--- monitoring/src/main/java/ch/unisg/monitoring/serialization/json/vgr/VgrEventDeserializer.java
+++ monitoring/src/main/java/ch/unisg/monitoring/serialization/json/vgr/VgrEventDeserializer.java
@@ -1,6 +1,5 @@
 package ch.unisg.monitoring.serialization.json.vgr;
 
-import ch.unisg.monitoring.domain.stations.VGR_1;
 import ch.unisg.monitoring.serialization.VgrEvent;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
@@ -12,12 +11,10 @@ import java.nio.charset.StandardCharsets;
 public class VgrEventDeserializer implements Deserializer<VgrEvent> {
     private final Gson gson =
         new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-    private static final Gson gsonVGR = new GsonBuilder()
-            .registerTypeAdapter(VGR_1.class, new VgrDeserializer())
-            .create();
+
     @Override
     public VgrEvent deserialize(String topic, byte[] bytes) {
         if (bytes == null) return null;
-        return gsonVGR.fromJson(new String(bytes, StandardCharsets.UTF_8), VgrEvent.class);
+        return gson.fromJson(new String(bytes, StandardCharsets.UTF_8), VgrEvent.class);
     }
 }
